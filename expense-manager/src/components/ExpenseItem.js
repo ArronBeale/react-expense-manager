99a78868a0dc019c2a647dfd685a45992dd957d0
Arron <beale86@msn.com>
2023-01-23 20:35:39 +0000

    Add ExpenseItem component

---
+++ expense-manager/src/components/ExpenseItem.js
@@ -0,0 +1,12 @@
+function ExpenseItem() {
+    return (
+    <div>
+        <div>March 17th 2021</div>
+            <div>
+                <h2>Car Insurance</h2>
+                <div>€294.55</div>
+            </div>
+    </div>);
+}
+
+export default ExpenseItem;
