e9e7b470ea41dbade7dbf069fbca59b11b506e49
Arron <beale86@msn.com>
2023-02-07 19:20:03 +0000

    Add ExpenseForm component to NewExpense, add NewExpense comonent to app.js

--- expense-manager/src/components/NewExpense/ExpenseForm.js
+++ expense-manager/src/components/NewExpense/ExpenseForm.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
@@ -15,6 +17,9 @@ const ExpenseForm = () => {
                 <label>Date</label>
                 <input type='date' min='2019-01-01' max='2023-12-31' />
             </div>
+        </div>
+        <div className='new-expense__actions'>
+            <button type='submit'>Add Expense</button>
         </div>
     </form>
 };
