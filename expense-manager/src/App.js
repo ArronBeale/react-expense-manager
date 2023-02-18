3d6992eeffbef8c3b2c86ed345b0d24bda105cc8
Arron <beale86@msn.com>
2023-02-18 15:25:17 +0000

    Add filter to enable user to filter expenses by year

--- expense-manager/src/App.js
+++ expense-manager/src/App.js
@@ -8,12 +8,12 @@ const DUMMY_EXPENSES = [
     id: 'e1',
     title: 'Bread (Brennans Sliced Pan)',
     amount: 94.12,
-    date: new Date(2020, 7, 14),
+    date: new Date(2023, 7, 14),
   },
   { id: 'e2',
   title: 'New TV',
   amount: 799.49,
-  date: new Date(2021, 2, 12)
+  date: new Date(2022, 2, 12)
 },
   {
     id: 'e3',
@@ -25,7 +25,7 @@ const DUMMY_EXPENSES = [
     id: 'e4',
     title: 'New Desk (Ikea standing desk)',
     amount: 450,
-    date: new Date(2021, 5, 12),
+    date: new Date(2020, 5, 12),
   },
 ];
 
